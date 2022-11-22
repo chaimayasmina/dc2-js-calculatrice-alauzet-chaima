dea0fda39c5ee4d12869590c1d0e3240f93f6e2a
Chaima Alauzet <alzt.chaima@gmail.com>
2022-11-22 11:20:10 +0100

    Ajout js

---
+++ script.js
@@ -0,0 +1,22 @@
+var a
+var b
+var result = 0
+
+function add(a, b){
+	result = a + b
+}
+
+function mul(a, b){
+	result = a * b
+}
+
+function subs(a, b){
+	result = a - b
+}
+
+function div(a, b){
+	result = a / b
+}
+
+add(1,1)
+console.log(result)
